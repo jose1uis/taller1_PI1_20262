11b1695bedd7ed2848678b42b91a7f248150c081
Jose Luis Restrepo <joseluisrestrepotamayo@gmail.com>
2026-02-05 16:47:40 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.html' )
-    return render(request, 'home.html', {'name':'Jose Luis Restrepo'})
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
+
+
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About page</h1>')
+    return render(request, 'about.html')
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
+# Build paths inside the project like this: os.path.join(BASE_DIR, ... )
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
